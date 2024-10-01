bb3d1d9699144da72f811593cf1e02a186eaba37
Korn6666 <simonmanes.sm@gmail.com>
2024-10-02 00:38:49 +0200

    camera follow

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Transform target;    // La voiture (ou l'objet que tu veux suivre)
+    public Vector3 offset;      // Décalage de la caméra par rapport à la voiture
+    public float smoothSpeed = 0.125f;  // Vitesse de transition lissée
+    private void Start()
     {
-        
+        offset = transform.position  - target.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Position souhaitée de la caméra
+        Vector3 desiredPosition = target.TransformPoint(offset);
+
+        // Interpolation de la position pour rendre le mouvement fluide
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        // Mise à jour de la position de la caméra
+        transform.position = new Vector3(smoothedPosition.x, offset.y + target.position.y, smoothedPosition.z) ;
+
+        // Orientation de la caméra vers l'objet suivi
+        transform.LookAt(target);
     }
 }
