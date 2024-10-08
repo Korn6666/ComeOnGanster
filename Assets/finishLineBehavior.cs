2522c4900edfd7b7a35c507672ab5b4ea9c84a39
Karel Jonquais <karel.jonquais@telecom-sudparis.eu>
2024-10-08 16:49:56 +0200

    QueAllezVousFaireLapTimeDansl'UI

---
+++ Assets/finishLineBehavior.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class finishLineBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private TextMeshProUGUI speedUI;
+    public int nLaps;
+    public float lapTime;
+
+    void OnTriggerEnter(Collider col){
+        nLaps +=1;
+        lapTime = 0;
+    }
+
+    void Update(){
+        Debug.Log("oui");
+        if(nLaps > 0) {
+            lapTime += Time.deltaTime;
+            Debug.Log(lapTime);
+            speedUI.text = "" + lapTime;
+        }
+
+    }
+}
